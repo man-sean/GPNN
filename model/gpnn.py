d3b2e01a4ba2ae1098b0f857299dba1277de0925
Sean Man <sean8100@gmail.com>
2022-02-28 12:26:14 +0200

    Fix future warning

--- model/gpnn.py
+++ model/gpnn.py
@@ -48,15 +48,14 @@ class gpnn:
         self.input_img = img_read(img_path)
         if config['out_size'] != 0:
             if self.input_img.shape[0] > config['out_size']:
-                self.input_img = rescale(self.input_img, config['out_size'] / self.input_img.shape[0],
-                                         multichannel=True)
+                self.input_img = rescale(self.input_img, config['out_size'] / self.input_img.shape[0], channel_axis=-1)
 
         # pyramids
         pyramid_depth = np.log(min(self.input_img.shape[:2]) / min(self.COARSE_DIM)) / np.log(self.R)
         self.add_base_level = True if np.ceil(pyramid_depth) > pyramid_depth else False
         pyramid_depth = int(np.ceil(pyramid_depth))
         self.x_pyramid = list(
-            tuple(pyramid_gaussian(self.input_img, pyramid_depth, downscale=self.R, multichannel=True)))
+            tuple(pyramid_gaussian(self.input_img, pyramid_depth, downscale=self.R, channel_axis=-1)))
         if self.add_base_level is True:
             self.x_pyramid[-1] = resize(self.x_pyramid[-2], self.COARSE_DIM)
         self.y_pyramid = [0] * (pyramid_depth + 1)
